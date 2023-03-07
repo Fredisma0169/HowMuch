f39171e9a162192342e539afdebfdd71215212a1
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-03-07 17:15:47 +0100

    Initial Commit

---
+++ HowMuch/HowMuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowMuchApp.swift
+//  HowMuch
+//
+//  Created by Fredisma OUEDRAOGO on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HowMuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
