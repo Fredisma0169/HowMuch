ea000cd2de5a991e25efe540bfd08947127bce2a
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-03-08 12:04:43 +0100

    Initial Commit

--- HowMuch/ContentView.swift
+++ HowMuch/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tipPct = 0
+    @State private var numPeople = 1
+    @State private var total = "0"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                    Text(total)
+                        .font(.system(size: 70))
+                        .frame(width: 260, alignment: .trailing)
+                        .padding(.vertical, 1)
+                    HStack {
+                        ForEach(7...9, id: \.self) { number in
+                            numberButton(number: "\(number)")
+                        }
+                    }
+                    HStack {
+                        ForEach(4...6, id: \.self) { number in
+                            numberButton(number: "\(number)")
+                        }
+                    }
+                    HStack {
+                        ForEach(1...3, id: \.self) { number in
+                            numberButton(number: "\(number)")
+                        }
+                    }
+                    HStack {
+                        numberButton(number: "0")
+                        numberButton(number: ",")
+                        Button {
+                            // Mark: action
+                        } label: {
+                            Image(systemName: "delete.backward.fill")
+                        }
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(width: 80, height: 80)
+                        .background(.red)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                    }
+                    HStack {
+                        Text("Tip")
+                        Picker(selection: $tipPct, label: Text("Tip")) {
+                            ForEach(0...100, id: \.self) {
+                                Text("\($0)%")
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                    HStack {
+                        Text("Number of People")
+                        Picker(selection: $numPeople, label: Text("Number of People")) {
+                            ForEach(0...20, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                    
+                    HStack {
+                        Button("Calculate") {
+                            //action
+                        }
+                        Button("clear") {
+                            //action
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+            .navigationTitle("Portion Calculator")
         }
-        .padding()
     }
 }
 
@@ -23,4 +90,22 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension ContentView {
+    
+    func numberButton(number: String) -> some View {
+        Button {
+            // action
+        } label: {
+            Text(number)
+        }
+        .font(.largeTitle)
+        .bold()
+        .frame(width: 80, height: 80)
+        .background(Color("AccentColor"))
+        .foregroundColor(.white)
+        .clipShape(Circle())
+    }
+    
 }
